91d455a7349ba02431bbd675621ec0e7c1252436
game4eyes <darkjjun68@gmail.com>
2024-04-16 10:34:52 +0900

    feat entity

--- src/main/java/com/travel/booking/domain/bus/BusEntity.java
+++ src/main/java/com/travel/booking/domain/bus/BusEntity.java
@@ -1,4 +1,18 @@
 package com.travel.booking.domain.bus;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
 public class BusEntity {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "")
+    private Long id;
 }
