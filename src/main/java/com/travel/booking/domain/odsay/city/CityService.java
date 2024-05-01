0043c07042c43c5c5fa55309a54376e178736768
namung08 <namung08@gmail.com>
2024-05-01 14:29:35 +0900

    feat: ApiDefault 클래스를 사용하여 통신

--- src/main/java/com/travel/booking/domain/odsay/city/CityService.java
+++ src/main/java/com/travel/booking/domain/odsay/city/CityService.java
@@ -1,5 +1,6 @@
 package com.travel.booking.domain.odsay.city;
 
+import com.travel.booking.domain.odsay.ApiDefault;
 import com.travel.booking.domain.odsay.OdsayConfig;
 import lombok.RequiredArgsConstructor;
 import org.json.simple.JSONArray;
@@ -26,45 +27,28 @@ public class CityService {
     // 도시 코드 얻기 위한 메소드
     public List<CityDTO> getCityList() {
         List<CityDTO> cityDtoList = new ArrayList<>();
+        StringBuilder urlBuilder =  new ApiDefault(odsayConfig).getURLBuilder("/searchCID",0,odsayConfig.getEtcKey());
         try {
-            StringBuilder urlBuilder = new StringBuilder(odsayConfig.getRequestURI());
-            urlBuilder.append("/searchCID");
-            urlBuilder.append("?"+ URLEncoder.encode("lang","UTF-8")+"=0");
-            urlBuilder.append("&"+ URLEncoder.encode("apiKey","UTF-8")+"="+URLEncoder.encode(odsayConfig.getEtcKey(),"UTF-8"));
-            URL url = new URL(urlBuilder.toString());
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
-            conn.setRequestProperty("Content-type", "application/json");
-            BufferedReader br = null;
-            String result;
-            if(conn.getResponseCode() >= 200 && conn.getResponseCode() < 300) {
-                br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-                result = br.readLine();
-                System.out.println(result);
-                JSONParser parser = new JSONParser();
-                JSONObject jsonObject = (JSONObject) parser.parse(result);
-                JSONObject resultJson = (JSONObject) jsonObject.get("result");
-                JSONArray cid = (JSONArray) resultJson.get("CID");
-                for (int i = 0; i < cid.size(); i++) {
-                    JSONObject cityJson = (JSONObject) cid.get(i);
-                    String cityName = (String) cityJson.get("cityName");
-                    String cityRegion = (String) cityJson.get("cityRegion");
-                    String cityCode = (String) cityJson.get("cityCode");
-                    CityDTO cityDTO = new CityDTO();
-                    cityDTO.setCityName(cityName);
-                    cityDTO.setCityRegion(cityRegion);
-                    cityDTO.setCityCode(cityCode);
-                    cityDtoList.add(cityDTO);
-                }
-                br.close();
-                conn.disconnect();
-                return cityDtoList;
-            } else {
-                br.close();
-                conn.disconnect();
-                return null;
+            String result = new ApiDefault(odsayConfig).getResult(urlBuilder);
+            System.out.println(result);
+            JSONParser parser = new JSONParser();
+            JSONObject jsonObject = (JSONObject) parser.parse(result);
+            JSONObject resultJson = (JSONObject) jsonObject.get("result");
+            JSONArray cid = (JSONArray) resultJson.get("CID");
+            for (int i = 0; i < cid.size(); i++) {
+                JSONObject cityJson = (JSONObject) cid.get(i);
+                String cityName = (String) cityJson.get("cityName");
+                String cityRegion = (String) cityJson.get("cityRegion");
+                String cityCode = (String) cityJson.get("cityCode");
+                CityDTO cityDTO = new CityDTO();
+                cityDTO.setCityName(cityName);
+                cityDTO.setCityRegion(cityRegion);
+                cityDTO.setCityCode(cityCode);
+                cityDtoList.add(cityDTO);
             }
-        } catch (IOException | ParseException e) {
+            return cityDtoList;
+        } catch (ParseException e) {
+            e.printStackTrace();
             throw new RuntimeException(e);
         }
     }
