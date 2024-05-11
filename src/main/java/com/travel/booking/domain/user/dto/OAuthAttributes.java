edc8f4087714d9380404c231e02e46ca8a3014ba
game4eyes <darkjjun68@gmail.com>
2024-05-11 22:17:43 +0900

    feat env file and social login

---
+++ src/main/java/com/travel/booking/domain/user/dto/OAuthAttributes.java
@@ -0,0 +1,62 @@
+package com.travel.booking.domain.user.dto;
+
+import com.travel.booking.domain.user.Role;
+import com.travel.booking.domain.user.entity.OauthUser;
+import com.travel.booking.domain.user.entity.UserEntity;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Map;
+
+@Getter
+public class OAuthAttributes {
+
+    private Map<String, Object> attributes;
+    private String nameAttributeKey;
+    private String name;
+    private String email;
+    private String picture;
+
+    @Builder
+    public OAuthAttributes(Map<String, Object> attributes,
+                           String nameAttributeKey, String name,
+                           String email, String picture) {
+        this.attributes = attributes;
+        this.nameAttributeKey = nameAttributeKey;
+        this.name = name;
+        this.email = email;
+        this.picture = picture;
+    }
+
+    // OAuth2User에서 반환하는 사용자 정보는 Map
+    // 따라서 값 하나하나를 변환해야 한다.
+    public static OAuthAttributes of(String registrationId,
+                                     String userNameAttributeName,
+                                     Map<String, Object> attributes) {
+
+        return ofGoogle(userNameAttributeName, attributes);
+    }
+
+    // 구글 생성자
+    private static OAuthAttributes ofGoogle(String usernameAttributeName,
+                                            Map<String, Object> attributes) {
+        return OAuthAttributes.builder()
+                .name((String) attributes.get("name"))
+                .email((String) attributes.get("email"))
+                .picture((String) attributes.get("picture"))
+                .attributes(attributes)
+                .nameAttributeKey(usernameAttributeName)
+                .build();
+    }
+
+    // User 엔티티 생성
+    public OauthUser toEntity() {
+        return OauthUser.builder()
+                .name(name)
+                .email(email)
+                .picture(picture)
+                .role(Role.USER)
+                .build();
+    }
+}
--- src/main/java/com/travel/booking/domain/user/repository/UserRepository.java
+++ src/main/java/com/travel/booking/domain/user/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package com.travel.booking.domain.user.repository;
 
+import com.travel.booking.domain.user.entity.OauthUser;
 import com.travel.booking.domain.user.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -9,4 +10,5 @@ public interface UserRepository extends JpaRepository<UserEntity, Long> {
     boolean existsByLoginId(String loginId);
     boolean existsByUsername(String username);
     Optional<UserEntity> findByLoginId(String loginId);
+    Optional<OauthUser> findByEmail(String email);
 }
