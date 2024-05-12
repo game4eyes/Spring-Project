5f17d621bbe0f4d86d170f85f214d3e6493def81
game4eyes <darkjjun68@gmail.com>
2024-05-12 15:57:16 +0900

    feat social-lolgin

--- src/main/java/com/travel/booking/security/cors/WebConfig.java
+++ src/main/java/com/travel/booking/security/cors/WebConfig.java
@@ -1,8 +1,12 @@
 package com.travel.booking.security.cors;
 
+import com.travel.booking.domain.user.resolver.LoginUserArgumentResolver;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -15,4 +19,10 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new LoginUserArgumentResolver());
+    }
+
 }
