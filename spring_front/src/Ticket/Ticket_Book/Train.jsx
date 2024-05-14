1a8a72cb58d7cacbfc00227377ad4ca5fe659f42
ljw <a@a.com>
2024-05-14 16:46:25 +0900

    Searching File complete

--- spring_front/src/Ticket/Ticket_Book/Train.jsx
+++ spring_front/src/Ticket/Ticket_Book/Train.jsx
@@ -4,197 +4,132 @@ import Article from '../../components/Article';
 import Ad from '../../components/Ad';
 import Footer from '../../components/Footer';
 import Charge from '../../components/Charge';
-//import SearchInput_Train from './components/SearchInput_Train';
 
-
-
-//양식 1
-// const Train = () => {
-//     const [departure, setDeparture] = useState('');
-//     const [destination, setDestination] = useState('');
-//     const [date, setDate] = useState('');
-//     const [time, setTime] = useState('');
-//     const [passengerCount, setPassengerCount] = useState('');
-
-//     // 출발지 검색어 변경 시 호출될 함수
-//     const handleDepartureChange = (value) => {
-//         console.log('출발지 검색어:', value);
-//         setDeparture(value);
-//     };
-
-//     // 도착지 검색어 변경 시 호출될 함수
-//     const handleDestinationChange = (value) => {
-//         console.log('도착지 검색어:', value);
-//         setDestination(value);
-//     };
-
-//     // 날짜 변경 시 호출될 함수
-//     const handleDateChange = (e) => {
-//         console.log('날짜:', e.target.value);
-//         setDate(e.target.value);
-//     };
-
-//     // 시간 변경 시 호출될 함수
-//     const handleTimeChange = (e) => {
-//         console.log('시간:', e.target.value);
-//         setTime(e.target.value);
-//     };
-
-//     // 인원 변경 시 호출될 함수
-//     const handlePassengerCountChange = (e) => {
-//         console.log('인원:', e.target.value);
-//         setPassengerCount(e.target.value);
-//     };
-
-//     const handleSubmit = (e) => {
-//         e.preventDefault();
-//         console.log({
-//             departure,
-//             destination,
-//             date,
-//             time,
-//             passengerCount
-//         });
-//     };
-
-//     return (
-//         <form onSubmit={handleSubmit}>
-//              <h4>기차 예약</h4>
-//             <label>
-//                 출발지:
-//                 <SearchInput_Train
-//                     placeholder="출발지를 입력하세요"
-//                     onInputChange={handleDepartureChange}
-//                 />
-//             </label>
-//             <br />
-//             <label>
-//                 도착지:
-//                 <SearchInput_Train
-//                     placeholder="도착지를 입력하세요"
-//                     onInputChange={handleDestinationChange}
-//                 />
-//             </label>
-//             <br />
-//             <label>
-//                 출발일:
-//                 <input type="date" onChange={handleDateChange} />
-//             </label>
-//             <br />
-//             <label>
-//                 시간:
-//                 <input type="time" onChange={handleTimeChange} />
-//             </label>
-//             <br />
-//             <label>
-//                 인원:
-//                 <input type="number" min="1" onChange={handlePassengerCountChange} />
-//             </label>
-//             <br />
-//             <button type="submit">예매하기</button>
-//         </form>
-//     );
-// };
-
-// export default Train;
-
-
-//양식 2
-
+// 기차 예약 컴포넌트
 const Train = () => {
-    const [departure, setDeparture] = useState('');
-    const [destination, setDestination] = useState('');
-    const [date, setDate] = useState('');
-    const [time, setTime] = useState('');
-    const [passengerCount, setPassengerCount] = useState('');
+    // 초기 상태 정의
+    const initState = {
+        departure: '',            // 출발지
+        destination: '',          // 도착지
+        date: '',                 // 출발일
+        passengerCount: '',       // 인원
+        isRoundTrip: false,       // 왕복 여부
+        selectedTrain: null,      // 선택된 기차 정보
+        isDepartureModalOpen: false // 출발지 모달 창 열림 여부
+    };
 
-    // 출발지 변경 시 호출될 함수
+    // 상태(state)와 상태를 변경하는 함수(setter) 정의
+    const [trainticket, setTrainticket] = useState(initState);
+
+    // 출발지 변경 이벤트 핸들러
     const handleDepartureChange = (e) => {
-        console.log('출발지:', e.target.value);
-        setDeparture(e.target.value);
+        setTrainticket(prevState => ({
+            ...prevState,
+            departure: e.target.value
+        }));
     };
 
-    // 도착지 변경 시 호출될 함수
+
+    
+    // 도착지 변경 이벤트 핸들러
     const handleDestinationChange = (e) => {
-        console.log('도착지:', e.target.value);
-        setDestination(e.target.value);
+        setTrainticket(prevState => ({
+            ...prevState,
+            destination: e.target.value
+        }));
     };
 
-    // 날짜 변경 시 호출될 함수
+    // 출발일 변경 이벤트 핸들러
     const handleDateChange = (e) => {
-        console.log('날짜:', e.target.value);
-        setDate(e.target.value);
+        setTrainticket(prevState => ({
+            ...prevState,
+            date: e.target.value
+        }));
     };
 
-    // 시간 변경 시 호출될 함수
-    const handleTimeChange = (e) => {
-        console.log('시간:', e.target.value);
-        setTime(e.target.value);
-    };
-
-    // 인원 변경 시 호출될 함수
+    // 인원 변경 이벤트 핸들러
     const handlePassengerCountChange = (e) => {
-        console.log('인원:', e.target.value);
-        setPassengerCount(e.target.value);
+        setTrainticket(prevState => ({
+            ...prevState,
+            passengerCount: e.target.value
+        }));
     };
 
+    // 팝업 열기 이벤트 핸들러
+    const openPopup = (stationClass,id) => () => {
+        const value = id === 'departure' ? trainticket.departure='departure': trainticket.destination='destination';
+        
+        const params = new URLSearchParams({
+            [id]: value,
+            stationClass: stationClass.toString(), 
+        });
+        window.open(`http://localhost:5173/search/searchtrain?${params}`, '_blank', 'width=600,height=400');
+    };
+
+    // 예약 양식 제출 핸들러
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log({
-            departure,
-            destination,
-            date,
-            time,
-            passengerCount
-        });
+        // TODO: 예약 정보 처리
     };
 
+
+    const changeDepartureDestination = ( ) => {
+        let tmp ="";
+        tmp=document.getElementById("departure").value;
+        document.getElementById("departure").value=document.getElementById("destination").value;
+        document.getElementById("destination").value = tmp;
+
+
+
+    }
+
+
+
+    // JSX 반환
     return (
         <div className="train_book">
         <form onSubmit={handleSubmit}>
-                <Header/>
-                <Article title ="기차 승차권 예매" body ="정보 입력"></Article>
+            <Header/>
+            <Article title="기차 승차권 예매" body="정보 입력" />
             <h4>기차 예약</h4>
+            {/* 출발지 입력란 */}
             <label>
                 출발지:
-                <select value={departure} onChange={handleDepartureChange}>
-                    <option value="">출발지를 선택하세요</option>
-                    <option value="서울">서울</option>
-                    <option value="부산">부산</option>
-                    <option value="대구">대구</option>
-                </select>
+                <input
+                    type="text"
+                    value={trainticket.departure}
+                    onChange={handleDepartureChange}
+                    placeholder="출발지를 입력하세요"
+                    onClick={openPopup('3','departure')} // 클릭한 대상의 ID를 전달
+                    id="departure"
+                />
             </label>
             <br />
+            {/* 도착지 입력란 */}
             <label>
                 도착지:
-                <select value={destination} onChange={handleDestinationChange}>
-                    <option value="">도착지를 선택하세요</option>
-                    <option value="서울">서울</option>
-                    <option value="부산">부산</option>
-                    <option value="대구">대구</option>
-                </select>
+                <input
+                    type="text"
+                    value={trainticket.destination}
+                    onChange={handleDestinationChange}
+                    placeholder="도착지를 입력하세요"
+                    onClick={openPopup('3','destination')} // 클릭한 대상의 ID를 전달
+                    id="destination"
+                />
             </label>
             <br />
+            <button type ="button" onClick={changeDepartureDestination}>출발지↔도착지</button>
+
+            {/* 출발일 선택란 */}
             <label>
                 출발일:
                 <input type="date" onChange={handleDateChange} />
             </label>
             <br />
-            {/* <label>
-                시간:
-                <select value={time} onChange={handleTimeChange}>
-                    <option value="">시간을 선택하세요</option>
-                    <option value="06:00">06:00</option>
-                    <option value="09:00">09:00</option>
-                    <option value="12:00">12:00</option>
-                    <option value="15:00">15:00</option>
-                    <option value="18:00">18:00</option>
-                </select>
-            </label>
-            <br />
+            {/* 인원 선택란 */}
             <label>
                 인원:
-                <select value={passengerCount} onChange={handlePassengerCountChange}>
+                <select value={trainticket.passengerCount} onChange={handlePassengerCountChange}>
                     <option value="">인원을 선택하세요</option>
                     <option value="1">1명</option>
                     <option value="2">2명</option>
@@ -202,15 +137,18 @@ const Train = () => {
                     <option value="4">4명</option>
                     <option value="5">5명</option>
                 </select>
-            </label> */}
+            </label>
             <br />
+            {/* 예약하기 버튼 */}
             <button type="submit">예매하기</button>
+            {/* 요금제 컴포넌트 */}
             <Charge id={2}/>
-
         </form>
-         <Ad/>
-         <Footer />
-         </div>
+        {/* 광고 컴포넌트 */}
+        <Ad/>
+        {/* 푸터 컴포넌트 */}
+        <Footer />
+    </div>
     );
 };
 
