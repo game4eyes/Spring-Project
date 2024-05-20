d9664349569cc80d172ffc9b45d8aef69d9f86eb
namung08 <namung08@gmail.com>
2024-05-20 15:50:12 +0900

    fix: 포트 추가

--- spring_front/src/api/dataApi.jsx
+++ spring_front/src/api/dataApi.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 
 
 
-export const API_SERVER_HOST = 'http://ec2-54-180-88-209.ap-northeast-2.compute.amazonaws.com'; // 서버 주소
+export const API_SERVER_HOST = 'http://ec2-54-180-88-209.ap-northeast-2.compute.amazonaws.com:9090'; // 서버 주소
 
 // 기본 주소 설정
 const apiPrefix = `${API_SERVER_HOST}/odsay/api`
