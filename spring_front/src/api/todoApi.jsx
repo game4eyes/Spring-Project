4620fc45b00e5a5e6fa0a97c7748bfbb2eaa904a
namung08 <namung08@gmail.com>
2024-05-23 15:33:32 +0900

    fix: cors 오류 해결

--- spring_front/src/api/todoApi.jsx
+++ spring_front/src/api/todoApi.jsx
@@ -2,9 +2,9 @@ import axios from "axios";
 
 // 서버 호스트 설정
 
-export const API_SERVER_HOST = 'http://localhost:9090'; // 서버 주소
+// export const API_SERVER_HOST = 'http://localhost:9090'; // 서버 주소
 
-//export const API_SERVER_HOST = 'ec2-15-164-224-69.ap-northeast-2.compute.amazonaws.com:9090';
+export const API_SERVER_HOST = 'http://ec2-15-164-224-69.ap-northeast-2.compute.amazonaws.com:9090'; // 서버 주소
 // 사용자 관련 엔드포인트S
 const userPrefix = `${API_SERVER_HOST}/api/user`;
 
