28fb2dffe47b5b7613f4f7b7fb74e664944f1cc5
namung08 <namung08@gmail.com>
2024-05-29 18:57:14 +0900

    feat: quary 기능 추가

--- src/main/java/com/travel/booking/domain/searchdb/repo/ScheduleRepository.java
+++ src/main/java/com/travel/booking/domain/searchdb/repo/ScheduleRepository.java
@@ -6,4 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
+
+    List<Schedule> findByStartStation_Id(Long id);
+
 }
