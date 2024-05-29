7719f732f47830d46b125479b2319382d85ea8c7
namung08 <namung08@gmail.com>
2024-05-29 16:19:06 +0900

    perf: odsay api를 사용하지 않음에 따른 사용하지 않는 class 삭제

--- src/main/java/com/travel/booking/domain/booking/entity/BookingEntityRepository.java
+++
@@ -1,5 +0,0 @@
-package com.travel.booking.domain.booking.entity;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookingEntityRepository extends JpaRepository<BookingEntity, Long> {}
