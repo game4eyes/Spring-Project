536838b271aa3adcacba08c3d8ecaabad0601944
namung08 <namung08@gmail.com>
2024-05-31 14:46:24 +0900

    del: change to entity

--- src/main/java/com/travel/booking/domain/searchdb/repo/SeatavailabilityRepository.java
+++
@@ -1,7 +0,0 @@
-package com.travel.booking.domain.searchdb.repo;
-
-import com.travel.booking.domain.searchdb.entity.Seatavailability;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SeatavailabilityRepository extends JpaRepository<Seatavailability, Long> {
-}
