7b88edd2ea7ca7d26a9a1b5b3a119bc22b977a8c
jyto <jyto0322@naver.com>
2024-06-04 02:16:19 +0900

    Footer 작업

--- spring_front/src/components/Footer/Location.jsx
+++ spring_front/src/components/Footer/Location.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
-import './Location.css';  // 스타일을 위한 CSS 파일
+import './Location.css';
 import Layout from '../Layout';
+
 
 const Location = () => {
   return (
     <div className="location-container">
         <Layout>
-            <h1>지옥</h1>
-            <p>우리의 주소를 찾으러 여기까지 왔다면 너는 너무 멀리 들어와버렸어. 지옥에 온걸 환영한다!</p>
-            <p>주소: 지옥의 끝자락</p>
+            <h1 className='hell-title'>Welcome to Hell</h1>
             <div className="button-container">
-                <button onClick={() => alert('영원히 함께하자')}>여기서 꺼내줘!</button>
+                <button onClick={() => alert('어디가...?')}>홈화면</button>
             </div>
         </Layout>
     </div>
