7b9ce8eff6070eb1bba7c8314d235fdd384f118a
namung08 <namung08@gmail.com>
2024-06-03 09:57:42 +0900

    feat: 주소 도메인으로 변경

--- spring_front/src/api/dataApi.jsx
+++ spring_front/src/api/dataApi.jsx
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 // export const API_SERVER_HOST = 'http://localhost:9090'; // 서버 주소
-export const API_SERVER_HOST = 'http://ec2-3-34-129-44.ap-northeast-2.compute.amazonaws.com:9090'; // 서버 주소
+export const API_SERVER_HOST = 'http://www.trable.kro.kr:9090/'; // 서버 주소
 
 
 // 기본 주소 설정
--- spring_front/src/api/todoApi.jsx
+++ spring_front/src/api/todoApi.jsx
@@ -3,9 +3,9 @@ import axios from "axios";
 // 서버 호스트 설정
 
 
-export const API_SERVER_HOST = 'http://localhost:9090'; // 서버 주소
+// export const API_SERVER_HOST = 'http://localhost:9090'; // 서버 주소
 
-//   export const API_SERVER_HOST = 'http://ec2-3-34-129-44.ap-northeast-2.compute.amazonaws.com:9090'; // 서버 주소
+  export const API_SERVER_HOST = 'http://http://www.trable.kro.kr:9090'; // 서버 주소
 
 
 // 사용자 관련 엔드포인트S
