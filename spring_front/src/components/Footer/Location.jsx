8891e208017bccdd8ef7108b3a226051a70007f9
jyto <jyto0322@naver.com>
2024-06-04 10:06:48 +0900

    Footer 찾아오시는길 완

--- spring_front/src/components/Footer/Location.jsx
+++ spring_front/src/components/Footer/Location.jsx
@@ -1,17 +1,31 @@
-import React from 'react';
+import { useEffect } from 'react';
 import './Location.css';
-import Layout from '../Layout';
-
+import { useNavigate } from 'react-router-dom';
 
 const Location = () => {
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    console.log("Adding full-height class");
+    document.documentElement.classList.add('full-height');
+    document.body.classList.add('full-height');
+  
+    return () => {
+      console.log("Removing full-height class");
+      document.documentElement.classList.remove('full-height');
+      document.body.classList.remove('full-height');
+    };
+  }, []);
+
+
   return (
-    <div className="location-container">
-        <Layout>
+    <div className="hell-location-container">
             <h1 className='hell-title'>Welcome to Hell</h1>
-            <div className="button-container">
+            <div className="hell-button-container">
                 <button onClick={() => alert('어디가...?')}>홈화면</button>
+                <button className="real-button" onClick={() => navigate('/')}>TRABLE</button>
             </div>
-        </Layout>
     </div>
   );
 }
